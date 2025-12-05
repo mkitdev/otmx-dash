01969c18b27145043a76aeead18fe69a96fa39e0
Kafka Hasan Maki <182825954+hasanmaki@users.noreply.github.com>
2025-12-05 15:26:11 +0700

    feat: implement database connection management

--- app/pages/settings.py
+++
@@ -1,9 +0,0 @@
-"""place holder settings page."""
-
-import streamlit as st
-
-from app.services.auth import require_login, require_role
-
-require_login()
-require_role("administrator")
-st.write("Ini adalah halaman Settings")
